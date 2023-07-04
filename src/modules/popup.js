01a8eac14abb11eb1ed75fe193a6ef3e8d6134b9
DevaWinner <winneraaniekan@gmail.com>
2023-07-04 17:35:48 +0100

    Add class to popup elements to enable styling

--- src/modules/popup.js
+++ src/modules/popup.js
@@ -2,26 +2,39 @@ const showPopup = (meal) => {
   const popup = document.createElement("div");
   popup.className = "popup";
 
-  const closePopupButton = document.createElement("button");
-  closePopupButton.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
-  closePopupButton.className = "close-popup-button";
-  popup.appendChild(closePopupButton);
-  
   const popupContent = document.createElement("div");
   popupContent.className = "popup-content";
+  
+  const popupHeading = document.createElement("div");
+  popupHeading.className = "popup-heading";
 
   const mealImage = document.createElement("img");
   mealImage.src = meal.strMealThumb;
   mealImage.alt = meal.strMeal;
-  popupContent.appendChild(mealImage);
+  popupHeading.appendChild(mealImage);
+
+  const closePopupButton = document.createElement("button");
+  closePopupButton.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
+  closePopupButton.className = "close-popup-button";
+  closePopupButton.classList.add("close-button");
+  popupHeading.appendChild(closePopupButton);
+
+  popupContent.appendChild(popupHeading);
+
+  const popupDescription = document.createElement("div");
+  popupDescription.className = "popup-description";
 
   const mealTitle = document.createElement("h2");
   mealTitle.textContent = meal.strMeal;
-  popupContent.appendChild(mealTitle);
+  mealTitle.className = "meal-title";
+  popupDescription.appendChild(mealTitle);
 
   const mealInstructions = document.createElement("p");
   mealInstructions.textContent = meal.strInstructions;
-  popupContent.appendChild(mealInstructions);
+  mealInstructions.className = "meal-instructions";
+  popupDescription.appendChild(mealInstructions);
+
+  popupContent.appendChild(popupDescription);
 
   closePopupButton.addEventListener("click", () => {
     popup.remove();
