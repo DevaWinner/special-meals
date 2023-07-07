8c2902c28448e16595a56666df14e8a831e10d6c
DevaWinner <winneraaniekan@gmail.com>
2023-07-07 11:28:04 +0100

    Create fake DOM for testing

--- test/comment.test.js
+++ test/comment.test.js
@@ -1,2 +1,21 @@
 import updateCommentCounter from '../src/modules/commentcounter.js';
 import { JSDOM } from 'jsdom';
+
+// Create a fake DOM for testing
+const dom = new JSDOM(`
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <title>Meal Finder</title>
+    </head>
+
+    <body>
+      <div id="popup"></div> <!-- Add the popup element here -->
+    </body>
+  </html>
+`);
+
+global.document = dom.window.document;
+global.window = dom.window;
+
