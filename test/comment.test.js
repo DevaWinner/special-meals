bfc91ed5d5be60d500759955e4c62885810c36a1
DevaWinner <winneraaniekan@gmail.com>
2023-07-07 11:28:26 +0100

    Mock fetch comments function

--- test/comment.test.js
+++ test/comment.test.js
@@ -19,3 +19,28 @@ const dom = new JSDOM(`
 global.document = dom.window.document;
 global.window = dom.window;
 
+// Mock the fetchComments function
+jest.mock('../src/api/fetchcomments.js', () => {
+  return jest.fn(() => {
+    return Promise.resolve([
+      {
+        id: 1,
+        username: 'John Doe',
+        comment: 'This is a comment',
+        creation_date: '2021-01-01 00:00:00',
+      },
+      {
+        id: 2,
+        username: 'Jane Doe',
+        comment: 'This is another comment',
+        creation_date: '2021-01-02 00:00:00',
+      },
+      {
+        id: 3,
+        username: 'John Smith',
+        comment: 'This is a third comment',
+        creation_date: '2021-01-03 00:00:00',
+      },
+    ]);
+  });
+});
