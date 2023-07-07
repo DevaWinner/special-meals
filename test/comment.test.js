253ec679c85b9e212adf094e4a30dedd13751d84
DevaWinner <winneraaniekan@gmail.com>
2023-07-07 11:32:52 +0100

    Solve linter error

--- test/comment.test.js
+++ test/comment.test.js
@@ -1,5 +1,5 @@
-import updateCommentCounter from '../src/modules/commentcounter.js';
 import { JSDOM } from 'jsdom';
+import updateCommentCounter from '../src/modules/commentcounter.js';
 
 // Create a fake DOM for testing
 const dom = new JSDOM(`
@@ -20,30 +20,26 @@ global.document = dom.window.document;
 global.window = dom.window;
 
 // Mock the fetchComments function
-jest.mock('../src/api/fetchcomments.js', () => {
-  return jest.fn(() => {
-    return Promise.resolve([
-      {
-        id: 1,
-        username: 'John Doe',
-        comment: 'This is a comment',
-        creation_date: '2021-01-01 00:00:00',
-      },
-      {
-        id: 2,
-        username: 'Jane Doe',
-        comment: 'This is another comment',
-        creation_date: '2021-01-02 00:00:00',
-      },
-      {
-        id: 3,
-        username: 'John Smith',
-        comment: 'This is a third comment',
-        creation_date: '2021-01-03 00:00:00',
-      },
-    ]);
-  });
-});
+jest.mock('../src/api/fetchcomments.js', () => jest.fn(() => Promise.resolve([
+  {
+    id: 1,
+    username: 'John Doe',
+    comment: 'This is a comment',
+    creation_date: '2021-01-01 00:00:00',
+  },
+  {
+    id: 2,
+    username: 'Jane Doe',
+    comment: 'This is another comment',
+    creation_date: '2021-01-02 00:00:00',
+  },
+  {
+    id: 3,
+    username: 'John Smith',
+    comment: 'This is a third comment',
+    creation_date: '2021-01-03 00:00:00',
+  },
+])));
 
 describe('updateCommentCounter', () => {
   beforeEach(() => {
@@ -84,7 +80,6 @@ describe('updateCommentCounter', () => {
       </div>
     `;
   });
-  
 
   test('should update comment counter with correct count', () => {
     updateCommentCounter();
